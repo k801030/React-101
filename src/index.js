1c596c7a4648bf20f4c28a836c979e4650a404b1
Vison <kk801030@gmail.com>
2018-01-08 18:08:24 +0800

    Embedding Expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom' 
 
+function formatName(user) {
+    return user.firstName + " " + user.lastName;
+}
+
+const user = {
+    firstName: "Vison",
+    lastName: "Li"
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+)
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 )
