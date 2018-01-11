cb546f53f122e389b366f5296948e6bebb35b03c
Vison <kk801030@gmail.com>
2018-01-11 15:51:37 +0800

    create a tick clock

--- src/index.js
+++ src/index.js
@@ -10,13 +10,24 @@ const user = {
     lastName: "Li"
 }
 
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
+
+const element2 = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, world! from React.createElement'
 )
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-)
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    )
+}
+
+setInterval(tick, 1000);
